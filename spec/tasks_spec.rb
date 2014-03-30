1e11a1352f536b721a9da84f1621e0b49f57bdd6
EmilyPlatzer <emilyplatzer@gmail.com>
2014-03-29 19:31:52 -0600

    User can add tasks and see them on the homepage
    Refreshing does not resubmit items

--- spec/tasks_spec.rb
+++ spec/tasks_spec.rb
@@ -4,8 +4,12 @@ require_relative '../task_app'
 Capybara.app = TaskApp
 
 feature "User can manage tasks" do
-  scenario "User is welcomed to homepage" do
+  scenario "User can add a task to the homepage" do
     visit '/'
+    click_on "Add a task"
+    fill_in "task", with: "Start a bike gang"
+    click_on "Create task"
     expect(page).to have_content("Welcome")
+    expect(page).to have_content("Start a bike gang")
   end
 end
--- task_app.rb
+++ task_app.rb
@@ -1,7 +1,15 @@
 require 'sinatra/base'
 
 class TaskApp < Sinatra::Application
+  TASKS = []
   get '/' do
-    "Welcome"
+    erb :index, locals: {:tasks => TASKS}
+  end
+  get'/new_task' do
+    erb :add_task
+  end
+  post '/' do
+    TASKS << params[:task]
+    redirect '/'
   end
 end
